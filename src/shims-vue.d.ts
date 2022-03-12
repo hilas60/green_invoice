b3632693c73db9bc4ac701b8d26424825745bcae
Hila Solomon <hilas60@gmail.com>
2022-03-12 16:24:55 +0200

    typescript fixes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,25 @@
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+import { Store } from "vuex";
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module "*.svg" {
+  import Vue, { VueConstructor } from "vue";
+  const content: VueConstructor<Vue>;
+  export default content;
+}
+
+declare module "@vue/runtime-core" {
+  // declare your own store states
+  interface State {
+    count: number;
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>;
+  }
 }
